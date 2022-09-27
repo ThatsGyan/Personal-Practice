676e758c08dc80afd3d27a1542c43a9b3de49451
GYANENDRA PAL SINGH <46261508+ThatsGyan@users.noreply.github.com>
2022-09-27 20:51:54 +0530

    Add files via upload

---
+++ 25SepInterfaces/25SepInterfaces/ITest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25SepInterfaces
+{
+    //<Modifire> interface <Name interface>
+    //{
+    //  -Abstract Member Decleration here;
+    //}
+    // default scope is public
+    //by default every member of interface is abstract .
+    //interface can inherit from another interface
+    interface ITest
+    {
+        void Add(int a, int b);   
+    }
+    interface ITest2:ITest
+    {
+        void sub(int a, int b);
+    }
+
+
+    class ImplementationClass :ITest
+    {
+         public void Add(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+    }
+    //createing object og an interface//
+
+
+
+}
---
+++ log4netpractice/log4netpractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4netpractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter 1st Number");
+           int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int result = n1 / n2;
+            Console.WriteLine("Devision Result : " result);
+        }
+    }
+}
